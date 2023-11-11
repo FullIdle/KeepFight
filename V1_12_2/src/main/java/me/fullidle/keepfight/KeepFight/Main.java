84405cfa0cf14315265e9392043a4518914dde9c
FullIdle <2675423979@qq.com>
2023-11-11 18:39:08 +0800

    奇葩版本,添加预用依赖

--- V1_12_2/src/main/java/me/fullidle/keepfight/KeepFight/Main.java
+++ V1_12_2/src/main/java/me/fullidle/keepfight/KeepFight/Main.java
@@ -3,82 +3,48 @@ package me.fullidle.keepfight.KeepFight;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.battles.BattleRegistry;
 import com.pixelmonmod.pixelmon.battles.controller.BattleControllerBase;
-import com.pixelmonmod.pixelmon.battles.controller.log.BattleActionBase;
-import com.pixelmonmod.pixelmon.battles.controller.participants.BattleParticipant;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PlayerParticipant;
 import com.pixelmonmod.pixelmon.comm.packetHandlers.battles.BackToMainMenu;
-import com.pixelmonmod.pixelmon.enums.battle.EnumBattleEndCause;
-import com.pixelmonmod.pixelmon.util.network.BetterNetworkWrapper;
+import lombok.SneakyThrows;
 import net.minecraft.entity.player.EntityPlayerMP;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Main extends JavaPlugin implements CommandExecutor {
+public class Main extends JavaPlugin implements Listener {
+    @SneakyThrows
     @Override
     public void onEnable() {
-        saveDefaultConfig();
-        getCommand("keepfight").setExecutor(this::onCommand);
-        getLogger().info("§3插件已载入");
+        getCommand("keepfight").setExecutor(this);
+        getLogger().info("插件已经载入!");
+        getServer().getPluginManager().registerEvents(this,this);
     }
 
+    @SneakyThrows
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            EntityPlayerMP player = Pixelmon.storageManager.getParty(p.getUniqueId()).getPlayer();
-            BattleControllerBase battle = BattleRegistry.getBattle(player);
-            if (battle != null) {
-                battle.pauseBattle();
-                battle.endPause();
-                battle.sendToAll(getMsg(getConfig().getString("Msg.KeepFightCmdMsg")));
-                switch (getConfig().getString("模式")){
-                    case "SIMPLE":{
-                        break;
-                    }
-                    case "UPDATE":{
-                        for (BattleParticipant participant : battle.participants) {
-                            participant.updateOtherPokemon();
-                            for (PixelmonWrapper wrapper : participant.allPokemon) {
-                                wrapper.update();
-                                wrapper.updateHPIncrease();
-                            }
-                        }
-                        BattleActionBase[] base = battle.battleLog.getAllActions().toArray(new BattleActionBase[0]);
-                        battle.battleLog.getAllActions().clear();
-                        for (BattleActionBase action : base) {
-                            battle.battleLog.addEvent(action);
-                        }
-                        battle.updatePokemonHealth();
-                        battle.update();
-                        break;
-                    }
-                    case "INTERFACE":{
-                        ArrayList<PixelmonWrapper> allPoke = battle.getTeamPokemon(battle.getParticipantForEntity(player));
-                        List<PixelmonWrapper> collect = allPoke.stream().filter(s -> s.getHealth() >= 1).collect(Collectors.toList());
-                        Pixelmon.network.sendTo(new BackToMainMenu(true,true, (ArrayList<PixelmonWrapper>) collect),player);
-                        break;
-                    }
-                }
-                battle.pauseBattle();
-                battle.endPause();
-        }else{
-                sender.sendMessage(getMsg(getConfig().getString("Msg.NotInToBattle")));
-            }
+        if (!(sender instanceof Player)){
+            sender.sendMessage("非玩家不可以用");
             return false;
         }
-        sender.sendMessage(getMsg(getConfig().getString("Msg.NoPlayer")));
+        Player player = (Player) sender;
+        EntityPlayerMP spe = Pixelmon.storageManager.getParty(player.getUniqueId()).getPlayer();
+        BattleControllerBase battle = BattleRegistry.getBattle(spe);
+        if (battle == null){
+            sender.sendMessage("§c你不在对局中!");
+            return false;
+        }
+        for (PlayerParticipant battlePlayer : battle.getPlayers()) {
+            List<PixelmonWrapper> list = Arrays.asList(battlePlayer.allPokemon);
+            battlePlayer.sendMessage(new BackToMainMenu(true, true, new ArrayList<>(list)));
+        }
         return false;
-    }
-
-    public String getMsg(String str){
-        return str.replace("&","§");
     }
 }
--- V1_16_5/src/main/java/me/fullidle/keepfight/KeepFight/Main.java
+++ V1_16_5/src/main/java/me/fullidle/keepfight/KeepFight/Main.java
@@ -1,90 +1,48 @@
 package me.fullidle.keepfight.KeepFight;
 
 import com.pixelmonmod.pixelmon.api.storage.StorageProxy;
-import com.pixelmonmod.pixelmon.api.util.helpers.NetworkHelper;
 import com.pixelmonmod.pixelmon.battles.BattleRegistry;
 import com.pixelmonmod.pixelmon.battles.controller.BattleController;
-import com.pixelmonmod.pixelmon.battles.controller.log.action.BattleAction;
-import com.pixelmonmod.pixelmon.battles.controller.participants.BattleParticipant;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
+import com.pixelmonmod.pixelmon.battles.controller.participants.PlayerParticipant;
 import com.pixelmonmod.pixelmon.comm.packetHandlers.battles.BackToMainMenuPacket;
+import lombok.SneakyThrows;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
-public class Main extends JavaPlugin implements CommandExecutor {
+public class Main extends JavaPlugin {
+    @SneakyThrows
     @Override
     public void onEnable() {
-        saveDefaultConfig();
-        getCommand("keepfight").setExecutor(this::onCommand);
-        getLogger().info("§3插件已载入");
+        getCommand("keepfight").setExecutor(this);
+        getLogger().info("插件已经载入!");
     }
 
+    @SneakyThrows
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            ServerPlayerEntity player = StorageProxy.getParty(p.getUniqueId()).getPlayer();
-            BattleController battle = BattleRegistry.getBattle(player);
-            if (battle != null) {
-                battle.pauseBattle();
-                battle.endPause();
-                battle.sendToAll(getMsg(getConfig().getString("Msg.KeepFightCmdMsg")));
-                switch (getConfig().getString("模式")){
-                    case "SIMPLE":{
-                        break;
-                    }
-                    case "UPDATE":{
-                        for (BattleParticipant participant : battle.participants) {
-                            participant.updateOtherPokemon();
-                            for (PixelmonWrapper wrapper : participant.allPokemon) {
-                                wrapper.update();
-                                wrapper.updateHPIncrease();
-                            }
-                        }
-                        BattleAction[] base = battle.battleLog.getAllActions().toArray(new BattleAction[0]);
-                        battle.battleLog.getAllActions().clear();
-                        for (BattleAction action : base) {
-                            battle.battleLog.logEvent(action);
-                        }
-                        battle.updatePokemonHealth();
-                        battle.update();
-                        break;
-                    }
-                    case "INTERFACE":{
-                        ArrayList<PixelmonWrapper> allPoke = battle.getTeamPokemon(battle.getParticipantForEntity(player));
-                        List<UUID> collect = allPoke.stream().map(s->{
-                            if (s.getHealth() >= 1) {
-                                return s.getPokemonUUID();
-                            }
-                            return null;
-                        }).filter(Objects::nonNull).collect(Collectors.toList());
-                        List<Boolean> booleans = collect.stream().map(s-> true).collect(Collectors.toList());
-                        NetworkHelper.sendPacket(player,new BackToMainMenuPacket(booleans,true,collect));
-                        break;
-                    }
-                }
-                battle.pauseBattle();
-                battle.endPause();
-        }else{
-                sender.sendMessage(getMsg(getConfig().getString("Msg.NotInToBattle")));
-            }
+        if (!(sender instanceof Player)){
+            sender.sendMessage("非玩家不可以用");
             return false;
         }
-        sender.sendMessage(getMsg(getConfig().getString("Msg.NoPlayer")));
+        Player player = (Player) sender;
+        ServerPlayerEntity spe = StorageProxy.getParty(player.getUniqueId()).getPlayer();
+        BattleController battle = BattleRegistry.getBattle(spe);
+        if (battle == null){
+            sender.sendMessage("§c你不在对局中!");
+            return false;
+        }
+        for (PlayerParticipant battlePlayer : battle.getPlayers()) {
+            List<PixelmonWrapper> list = Arrays.asList(battlePlayer.allPokemon);
+            battlePlayer.sendMessage(new BackToMainMenuPacket(list.stream().map(r-> (r.isAlive()&&r.entity == null)).collect(Collectors.toList()), true, list.stream().map(PixelmonWrapper::getPokemonUUID).collect(Collectors.toList())));
+        }
         return false;
-    }
-
-    public String getMsg(String str){
-        return str.replace("&","§");
     }
 }
